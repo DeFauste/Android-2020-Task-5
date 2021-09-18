e23e47a40b36d8c6b95d084de8c29a5350ffe7f3
debran <tophadoziratu@gmail.com>
2021-09-18 22:21:34 +0300

    feat: add basic page layout

--- app/src/main/java/com/example/luckycat/MainActivity.kt
+++ app/src/main/java/com/example/luckycat/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.luckycat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_navigation) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_navigation) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
